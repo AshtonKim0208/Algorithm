11b17307c6bdaa0104957fc346b6fc900be325e5
seogkim <seogkim@student.42seoul.kr>
2020-07-29 09:45:09 +0900

    요세푸스 문제 해야함

--- Algorithm/42ALGO/level_1/요세푸스 문제.cpp
+++ Algorithm/42ALGO/level_1/요세푸스 문제.cpp
@@ -1,28 +1,29 @@
 #include <cstdio>
 int main(void)
 {
-    int N, K, flag[5001] = {0, }, answer[5001] = {0, };
+    int N, K, used[5001]={0, }, answer[5001];
+
     scanf("%d %d", &N, &K);
-    int p = 0, idx = 0;
-    while (1)
+    int i = 0, n = 0;;
+    for (int i = 0; i < N; )
     {
-        if (idx == N)
-            break ;
-        if (flag[(p+K) % N] == 0)
-        {
-            answer[idx++] = (p+K) % N == 0 ? p+K : (p+K)%N;
-            flag[(p+K) % N] = 1;
-            p += K;
-            if (p >= N) p %= N;
+        int idx = 0, plus = 0;
+        while (idx < 3) {
+            if (used[(plus+K+n)%N] == 0)
+                idx++;
+            else
+                plus++;
         }
-        else
-        {
-            p++;
+        n += plus;
+        n %= N;
+        if (used[(n+K)%N] == 0) {
+            used[(n+K)%N] = 1;
+            answer[i++] = (n+K)%N;
         }
     }
     printf("<");
-    for (int i =0; i < N-1; i++)
-        printf("%d, ", answer[i]);
-    printf("%d>\n", answer[N-1]);
+    for (int i=0; i<N-1;i++)
+        printf("%d, ");
+    printf("%d>");
     return 0;
 }
